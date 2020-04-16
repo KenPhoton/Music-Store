e125a74022bac85be07a15f47e49d0b88e1535fe
Jade Bowyer <celestialblue13@knights.ucf.edu>
2020-04-15 21:57:19 -0400

    possible shell improvement

--- Server/check.sh
+++ Server/check.sh
@@ -3,27 +3,23 @@
 # Run this on the server; checks for updates every ~10 seconds
 
 cd /var/www/html/Music-Store
+git fetch
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
 
-while true; do
-    git fetch
-	UPSTREAM=${1:-'@{u}'}
-	LOCAL=$(git rev-parse @)
-	REMOTE=$(git rev-parse "$UPSTREAM")
-	BASE=$(git merge-base @ "$UPSTREAM")
-
-	if [ $LOCAL = $REMOTE ]; then
-		echo "Up-to-date"
-	elif [ $LOCAL = $BASE ]; then
-		echo "Need to pull"
-		git pull
-		rm -f /var/www/html/*
-		sudo cp /var/www/html/Music-Store/Frontend/* /var/www/html/
-		sudo cp /var/www/html/Music-Store/API/* /var/www/html/
-	elif [ $REMOTE = $BASE ]; then
-		echo "Need to push"
-	else
-		echo "Diverged"
-		git pull # Handle divergence ?
-	fi
-	sleep 120 # Re-check in 10 seconds
-done
+if [ $LOCAL = $REMOTE ]; then
+	echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+	echo "Need to pull"
+	git pull
+	rm -f /var/www/html/*
+	sudo cp /var/www/html/Music-Store/Frontend/* /var/www/html/
+	sudo cp /var/www/html/Music-Store/API/* /var/www/html/
+elif [ $REMOTE = $BASE ]; then
+	echo "Need to push"
+else
+	echo "Diverged"
+	git pull # Handle divergence ?
+fi
