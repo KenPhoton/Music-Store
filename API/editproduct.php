32ff0ee0fd8f0966e800d903b4acebb362d5266e
Jade Bowyer <celestialblue13@knights.ucf.edu>
2020-04-19 19:57:06 -0400

    editproduct?

---
+++ API/editproduct.php
@@ -0,0 +1,64 @@
+<?php
+    $servername = "ec2-52-2-112-101.compute-1.amazonaws.com";
+    $username = "testuser";
+    $password = "jo42hn25yhf92cu";
+    $dbname = "discount_db";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+    $conn = new mysqli("poosddb.ckbkojoxq1y0.us-east-1.rds.amazonaws.com", "poosdAdmin", "DontForgetThis321", "poosdDB");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+        $_POST = json_decode(file_get_contents('php://input'), true);
+        $productid = $_POST['productid'];
+        $productname = $_POST["productname"];
+        $fullprice = $_POST["fullprice"];
+        $description = $_POST["description"];
+        $category = $_POST["category"];
+        $stocked = $_POST["stocked"];
+        $picname = $_POST["picname"];
+
+        $sql = "UPDATE Product SET productname='" . $productname . "', fullprice='" . $fullprice . "', description='" . $description . "', category='" . $category . "', stocked='" . $stocked . "' WHERE productid='" . $productid . "'";
+        if ($conn->query($sql) != TRUE)
+        {
+            $conn->close();
+            returnWithError( "Error updating product" );
+        }
+        else
+        {
+            if ($picname != "") {
+                $sql = "UPDATE Product SET picname='" . $picname . "' WHERE productid='" . $productid . "'";
+                if ($conn->query($sql) != TRUE)
+                {
+                    $conn->close();
+                    $message = '{"error":"", "result":"edited product"}';
+                    sendResultInfoAsJson($message);
+                }
+                else
+                {
+                    $conn->close();
+                    returnWithError( "Error updating product" );
+                }
+            }
+            $conn->close();
+            $message = '{"error":"", "result":"edited product"}';
+            sendResultInfoAsJson($message);
+        }
+    }
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
