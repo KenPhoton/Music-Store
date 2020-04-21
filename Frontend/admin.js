cf6cf7c8f45878080a3de33c2118dcee9bdf8a2f
Jade Bowyer <celestialblue13@knights.ucf.edu>
2020-04-20 20:42:32 -0400

    typo

--- Frontend/admin.js
+++ Frontend/admin.js
@@ -236,7 +236,7 @@ function searchPurchaseHistory()
                     var table = document.getElementById("purchaseList");
                     table.deleteTHead();
                     var newPurchase = table.createTHead();
-                    newPurchase.outerHTML='<thead class="thead-light"><colgroup><col span="1" style="width: 3%;"><col span="1" style="width: 3%;"><col span="1" style="width: 5%;"><col span="1" style="width: 15%;"><col span="1" style="width: 21%;"><col span="1" style="width: 33%;"><col span="1" style="width: 20;"></colgroup></thead>';
+                    newPurchase.outerHTML='<thead class="thead-light"><colgroup><col span="1" style="width: 3%;"><col span="1" style="width: 3%;"><col span="1" style="width: 5%;"><col span="1" style="width: 15%;"><col span="1" style="width: 21%;"><col span="1" style="width: 33%;"><col span="1" style="width: 20%;"></colgroup></thead>';
                     
                     for (var i = jsonObject.results.length - 1; i >= 0; i--)
                     {
@@ -364,7 +364,7 @@ function getDiscounts()
                     var table = document.getElementById("discountList");
 					table.deleteTHead();
                     var newDiscount = table.createTHead();
-                    newDiscount.outerHTML='<thead class="thead-light"><colgroup><col span="1" style="width: 10%;"><col span="1" style="width: 10%;"><col span="1" style="width: 20%;"><col span="1" style="width: 20%;"><col span="1" style="width: 10%;"><col span="1" style="width: 10;"><col span="1" style="width: 20;"></colgroup></thead>';
+                    newDiscount.outerHTML='<thead class="thead-light"><colgroup><col span="1" style="width: 10%;"><col span="1" style="width: 10%;"><col span="1" style="width: 20%;"><col span="1" style="width: 20%;"><col span="1" style="width: 10%;"><col span="1" style="width: 10%;"><col span="1" style="width: 20%;"></colgroup></thead>';
                     
                     for (var i = jsonObject.results.length - 1; i >= 0; i--)
                     {
