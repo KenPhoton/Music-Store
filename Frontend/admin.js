416bc48be20fb964cad2471203b782bdeff0f38c
Jade Bowyer <celestialblue13@knights.ucf.edu>
2020-04-21 00:30:53 -0400

    meep

--- Frontend/admin.js
+++ Frontend/admin.js
@@ -252,7 +252,7 @@ function searchPurchaseHistory()
                     var table = document.getElementById("purchaseList");
                     table.deleteTHead();
                     var newPurchase = table.createTHead();
-                    newPurchase.outerHTML='<thead class="thead-light"><colgroup><col span="1" style="width: 15%;"><col span="1" style="width: 15%;"><col span="1" style="width: 20%;"><col span="1" style="width: 30%;"><col span="1" style="width: 20%;"></colgroup></thead>';
+                    newPurchase.outerHTML='<thead class="thead-light"><colgroup><col span="1" style="width: 15%;"><col span="1" style="width: 15%;"><col span="1" style="width: 20%;"><col span="1" style="width: 30%;"><col span="1" style="width: 20%;"></colgroup></>';
                     
                     for (var i = jsonObject.results.length - 1; i >= 0; i--)
                     {
